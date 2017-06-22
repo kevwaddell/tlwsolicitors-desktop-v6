02220f8fa086488bf24398f58a8a5fb7dfd931b6
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-06-22 15:31:12 +0100

    Take back slash out of end of contact link in header

--- header-landing.php
+++ header-landing.php
@@ -49,8 +49,8 @@ $contact_pg = get_page_id( 'contact-us' );
 					<div class="col-xs-6">
 						<div class="header-action-btns pull-right">
 							<a href="<?php echo get_option('home'); ?>/" class="btn btn-default"><span class="sr-only"><?php bloginfo('name'); ?></span><i class="fa fa-home fa-lg"></i></a>
-							<a href="<?php echo get_permalink($contact_pg); ?>/" class="btn btn-default"><span class="sr-only"><?php get_the_title($contact_pg); ?></span><i class="fa fa-paper-plane fa-lg"></i></a>
-							<a href="<?php echo get_permalink($contact_pg); ?>/#find-us" class="btn btn-default"><span class="sr-only">Find us</span><i class="fa fa-map-marker fa-lg"></i></a>
+							<a href="<?php echo get_permalink($contact_pg); ?>" class="btn btn-default"><span class="sr-only"><?php get_the_title($contact_pg); ?></span><i class="fa fa-paper-plane fa-lg"></i></a>
+							<a href="<?php echo get_permalink($contact_pg); ?>#find-us" class="btn btn-default"><span class="sr-only">Find us</span><i class="fa fa-map-marker fa-lg"></i></a>
 							<a href="mailto:<?php echo $email;?>" class="btn btn-default"><span class="sr-only">Email us</span><i class="fa fa-envelope fa-lg"></i></a>
 						</div>
 					</div>
