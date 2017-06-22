00cdde887ec50692801b61784dce8c4c64a8b38c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-06-22 16:55:29 +0100

    Hide vacancies form when position is closed.

--- parts/vacancies/section-form.php
+++ parts/vacancies/section-form.php
@@ -1,17 +1,22 @@
 <?php
 global $jobs_pg;
+$today = date('Ymd');
+
+
 if (is_post_type_archive()) {
 $form = get_field('form', $jobs_pg->ID);	
 $section_title = get_field('form_section_title', $jobs_pg->ID);
 } else {
 $form = get_field('form');	
-$section_title = get_field('form_section_title');	
+$section_title = get_field('form_section_title');
+$job_display_to_date = get_field( 'job_display_to_date');	
 }
 	
 if (empty($section_title)) {
 $section_title = $form->title;
 }	
 ?>
+<?php if (is_post_type_archive() || $job_display_to_date > $today) { ?>
 <div class="rule"></div>
 <section id="form-section" class="pg-section with-header">
 	<div class="container">
@@ -20,3 +25,4 @@ $section_title = $form->title;
 		<?php gravity_form($form->id, false, true, true, null, true); ?>
 	</div>
 </section>
+<?php } ?>
