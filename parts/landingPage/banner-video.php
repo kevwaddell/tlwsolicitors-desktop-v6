e213e151b7c813aeaa4f0e04c0942a13afbd7081
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-03 12:24:04 +0100

    Fix size on video banner.

--- parts/landingPage/banner-video.php
+++ parts/landingPage/banner-video.php
@@ -12,11 +12,12 @@ $poster_full_src = wp_get_attachment_image_src($poster_img_id, 'full' );
 
 		<div class="row">			
 			<div class="col-xs-10 col-xs-offset-1">
-				<video controls preload="auto" id="service-video" poster="<?php echo $poster_full_src[0];?>">
-					<source src="<?php echo $video_webm; ?>" type="video/webm" />
-					<source src="<?php echo $video_mp4; ?>" type="video/mp4" />
-				</video>
-				
+				<div class="video-wrapper">
+					<video controls preload="auto" id="service-video" poster="<?php echo $poster_full_src[0];?>">
+						<source src="<?php echo $video_webm; ?>" type="video/webm" />
+						<source src="<?php echo $video_mp4; ?>" type="video/mp4" />
+					</video>
+				</div>
 				<header class ="banner-title font-slab-serif caps text-center">
 						<span><?php the_title(); ?></span>
 				</header>
