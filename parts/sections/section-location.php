d8cca0663e5c31ea3a39234693c2f297f7929ee3
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-08-29 15:18:56 +0100

    Fix location map

--- parts/sections/section-location.php
+++ parts/sections/section-location.php
@@ -17,6 +17,37 @@ $map_active = $section['map_active'];
 //echo '<pre>';print_r($address);echo '</pre>';
 	$map_marker = get_stylesheet_directory_uri()."/_/img/map-marker.png";
 ?>
+<div class="rule"></div>
+<section id="wide-map" class="pg-section">
+	<a id="find-us" name="find-us"></a>
+	
+	<div class="container">
+		
+		<h2 class="section-header"><?php echo $section_title; ?></h2>
+		<div class="row">
+			<div class="col-xs-3">
+				<div class="location-box">
+					<?php foreach ($options as $op) { ?>
+						<?php if ($op == 'address') { ?>
+							<address>
+							<span class="company bold caps col-red font-size-25"><?php bloginfo('name'); ?></span><br>
+							<?php echo $address; ?>
+							</address>	
+						<?php } ?>
+						<?php if ($op == 'route') { ?>
+						<button id="route-finder-btn" class="btn btn-block"><i class="fa fa-car"></i> Route finder</button>			
+						<?php } ?>	
+					<?php } ?>
+				</div>
+			</div>
+			<div class="col-xs-9">
+				<div id="wide-map-canvas"></div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
 <script>
 function wide_map_init() {
 var TLW_MAP_ID = 'TLW_style';
@@ -51,35 +82,5 @@ marker = new google.maps.Marker({position: myLatLang,map: wide_map,icon: image,t
 };
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWfwM9bo9zsSfrpXwMZ0bDZbhvgiP1tKo&callback=wide_map_init" async defer></script>
-<div class="rule"></div>
-<section id="wide-map" class="pg-section">
-	<a id="find-us" name="find-us"></a>
-	
-	<div class="container">
-		
-		<h2 class="section-header"><?php echo $section_title; ?></h2>
-		<div class="row">
-			<div class="col-xs-3">
-				<div class="location-box">
-					<?php foreach ($options as $op) { ?>
-						<?php if ($op == 'address') { ?>
-							<address>
-							<span class="company bold caps col-red font-size-25"><?php bloginfo('name'); ?></span><br>
-							<?php echo $address; ?>
-							</address>	
-						<?php } ?>
-						<?php if ($op == 'route') { ?>
-						<button id="route-finder-btn" class="btn btn-block"><i class="fa fa-car"></i> Route finder</button>			
-						<?php } ?>	
-					<?php } ?>
-				</div>
-			</div>
-			<div class="col-xs-9">
-				<div id="wide-map-canvas"></div>
-			</div>
-		</div>
-	</div>
-
-</section>
 
 <?php } ?>
