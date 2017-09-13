cbbabbf15090f7df0c8d6a8aa1be51776713a25b
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-09-13 09:22:52 +0100

    Turn off ajax on home page form

--- parts/sections/home/section-form.php
+++ parts/sections/home/section-form.php
@@ -8,9 +8,7 @@ $form = get_field('hp_form', 'option');
 ?>
 <section id="hp-form" class="pg-section">
 	<div class="container">
-		<?php 
-		gravity_form($form->id, false, true, false, '', true);	
-		 ?>
+		<?php gravity_form($form->id, false, true, false, '', false); ?>
 	</div>
 </section>
 <?php } ?>
