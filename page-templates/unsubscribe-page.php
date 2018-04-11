c41ca0c82aa2afa1e5c1a69452c5a87d7e858217
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-04-11 12:23:51 +0100

    Add unsubscribe page.

--- page-templates/unsubscribe-page.php
+++ page-templates/unsubscribe-page.php
@@ -31,7 +31,14 @@ Template Name: Newsletter unsubscribe template
 					<div class="main-txt">
 		
 						<?php the_content(); ?>
-	
+						
+						<?php if (isset($_GET['unsubscribed'])) { ?>
+							<div class="alert alert-success text-center">
+							<h2 class="text-success caps">Contact removal successful!</h2>	
+							<p>You have unsubscribed from the TLW Solicitors email marketing list.</p>
+							<p><a href="<?php echo get_option('home'); ?>/" class="btn btn-success btn-lg txt-col-wht">Continue&nbsp;&nbsp;&nbsp;<i class="fa fa-angle-right"></i></a></p>
+							</div>		
+						<?php } else { ?>
 						<!-- Start of unsubscribe -->
 						<script language="javascript">
 						<!-- function validate_unsubscribe(frm) { var isError = false; var emailAddress = frm.Email.value; if (emailAddress == '' || emailAddress.indexOf('@') == -1) { alert('Please enter your email address'); isError = true; } return !isError; } //--><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span>
@@ -52,6 +59,7 @@ Template Name: Newsletter unsubscribe template
 								<input class="btn btn-primary btn-lg" name="Submit" type="Submit" value="Unsubscribe" />
 							</form>
 							</div>
+						<?php } ?>
 		
 					</div>
 																
