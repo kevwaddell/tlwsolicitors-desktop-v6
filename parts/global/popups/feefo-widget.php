2724688ea7c345034499c47921802c76f93a5759
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-07-03 15:33:49 +0100

    async Feefo

--- parts/global/popups/feefo-widget.php
+++ parts/global/popups/feefo-widget.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="https://api.feefo.com/api/javascript/tlw-solicitors"></script>
+<script async type="text/javascript" src="https://api.feefo.com/api/javascript/tlw-solicitors"></script>
 <div id="feefo-service-review-floating-widgetId"></div>
