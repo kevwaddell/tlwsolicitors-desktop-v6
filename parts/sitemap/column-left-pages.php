2005139290325648b25f12ce280bddcfcaeadfc2
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-10-16 17:05:55 +0100

    New structure changes

--- parts/sitemap/column-left-pages.php
+++ parts/sitemap/column-left-pages.php
@@ -1,86 +1,47 @@
 <?php
-$services_pgs = get_field('global_main_services', 'options');
-//echo '<pre>';print_r($services_pgs);echo '</pre>';
-
-$services_args = array(
-'post_type'		=> 'page',
-'orderby'		=> 'menu_order',
-'include'		=> $services_pgs,
-'order'			=> 'ASC'
-);
-
-$services = get_posts($services_args);
-//echo '<pre>';print_r($landing_pgs);echo '</pre>';
+$gbl_services_2018 = get_field( 'gbl_services_2018', 'options' );
+//echo '<pre>';print_r($gbl_services_2018);echo '</pre>';
 ?>
 
 <div class="col-xs-6">
-
-	<?php if ($services) { ?>
+<?php if (!empty($gbl_services_2018)) { ?>
 	
-	<?php foreach ($services as $service) { ?>
-	
-	<?php 
-	$service_args = array(
-	'posts_per_page' => -1,
-	'post_type'		=> 'page',
-	'orderby'		=> 'menu_order',
-	'post_parent'	=> $service->ID,
-	'order'			=> 'ASC'
-	);
-	
-	$service_children = get_posts($service_args);
-	
-	$landing_pg_args = array(
-	'post_type'		=> 'tlw_landing_page',
-	'meta_key'	=> 'service_area',
-	'meta_value'=> $service->ID,
-	'order'			=> 'ASC'
-	);
-	$landing_pgs = get_posts($landing_pg_args);
-	
-	//echo '<pre>';print_r($landing_pgs);echo '</pre>';
-	
-	if (!empty($landing_pgs)) {
-		foreach ($landing_pgs as $lp) {
-		array_unshift($service_children, $lp);
-		} 	
-	}
-	 ?>
-	
-		<div class="list-block">
-			<a href="<?php echo get_permalink($service->ID); ?>" class="header-link"><?php echo $service->post_title; ?></a>
+	<?php foreach ($gbl_services_2018 as $s) { 
+	$title_type = $s['title_type'];	
+	$sub_pages = $s['sub_pages'];
+	//echo '<pre>';print_r($s);echo '</pre>';
+	?>
+	<div class="list-block">
 			
-		<?php if ($service_children) { ?>
+			<?php if ($title_type) { 
+			$service_text = $s['service_text'];
+			?>
+			<h3><?php echo $service_text; ?></h3>
+			<?php } else { 
+			$service_pg = $s['service_pg'];
+			?>
+			<a href="<?php echo get_permalink($service_pg); ?>" class="header-link"><?php echo get_the_title( $service_pg ); ?></a>
+			<?php } ?>
+			
+			<?php if ($sub_pages) { 
+			$sub_services = $s['services'];
+			?>
 			
 			<ul class="list-unstyled">
 			
-			<?php foreach ($service_children as $service_child) { 
-			$gc_args = array(
-			'posts_per_page' => -1,
-			'post_type'		=> 'page',
-			'orderby'		=> 'menu_order',
-			'post_parent'	=> $service_child->ID,
-			'order'			=> 'ASC'
-			);
-			$service_gchildren = get_posts($gc_args);
+			<?php foreach ($sub_services as $sc) { 
+			$sc_id = $sc['service'];
 			?>
-			<li><a href="<?php echo get_permalink($service_child->ID); ?>"><?php echo $service_child->post_title; ?></a></li>
-				<?php if ($service_gchildren) { ?>
-					<?php foreach ($service_gchildren as $g_child) { ?>
-					<li><a href="<?php echo get_permalink($g_child->ID); ?>"><?php echo get_the_title($g_child->ID); ?></a></li>
-					<?php } ?>
-				<?php } ?>
-
+			<li><a href="<?php echo get_permalink($sc_id); ?>"><?php echo get_the_title($sc_id); ?></a></li>
 			<?php } ?>
 				
 			</ul>	
 			
 		<?php } ?>
-
-		</div>
-	
+		
+	</div>
 	<?php } ?>
 
-	<?php } ?>
-					
+<?php } ?>
+				
 </div>
