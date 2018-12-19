abe244acedd7ef2ea7d8621f51f71603cf1ad8a0
Kevin TLW <kwaddell@tlwsolicitors.co.uk>
2018-12-19 10:10:39 +0000

    Fix header

--- parts/headers/header-global.php
+++ parts/headers/header-global.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-GB" class="no-js">
 <head id="www-tlwsolicitors-co-uk" data-template-set="tlw-solicitors-theme">
-<script type="text/javascript">
-    var elm=document.getElementsByTagName("html")[0];
-    elm.style.display="none";
-</script>	
+
 	<?php if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') { ?>
 	<!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PLBR4F');</script>
@@ -36,28 +33,5 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	
 	<?php wp_head(); ?>
-	
-	<script>
-		var loadDeferredStyles = function() {
-		var addStylesNode = document.getElementById("deferred-styles");
-		var gfStylesNode = document.getElementById("gf-deferred-styles");
-		var replacement = document.createElement("div");
-		var elm=document.getElementsByTagName("html")[0];
-		replacement.innerHTML = gfStylesNode.textContent + addStylesNode.textContent;
-		document.body.appendChild(replacement);
-		gfStylesNode.parentElement.removeChild(gfStylesNode);
-		addStylesNode.parentElement.removeChild(addStylesNode);
-		elm.style.display="block";
-		};
-	 
-		var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-	  if (raf) { 
-		  raf(function() { 
-		   window.setTimeout(loadDeferredStyles, 0); 
-			});
-	 	} else { 
-		 	window.addEventListener('load', loadDeferredStyles); 
-		 }
-	</script>
 	
 </head>
